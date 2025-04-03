5774b22eed95769d03f43465910b7c85885fc229
singhbanta <singhbanta84@gmail.com>
2025-04-04 00:03:28 +0530

    add: another usage of static keyword

--- 18StaticKeyword.cpp
+++ 18StaticKeyword.cpp
@@ -15,3 +15,29 @@ int main(){
     return 0;
 
 }
+
+
+//Example another usage of static keyword.
+
+#include <iostream>
+#include <string>   
+using namespace std;
+
+
+class ABC{
+    public:
+    ABC(){
+        cout<<"Constructor called"<<endl;
+    }
+
+    ~ABC(){
+        cout<<"Destructor called"<<endl;
+    }
+};
+int main(){
+    if(true){
+        static ABC obj;//Static object is created in the static memory area and it is destroyed when the program ends.
+        cout<<"end of function call"<<endl;
+    }
+
+}
